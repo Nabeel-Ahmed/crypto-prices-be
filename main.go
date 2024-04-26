27cc5a367e60eb8740729d0ee98826c1cb039b28
Nabeel Ahmed <38479568+Nabeel-Ahmed@users.noreply.github.com>
2024-04-26 23:31:29 +0100

    feat: :sparkles: Add route and template for main website

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
 	"github.com/nabeel-ahmed/crypto-prices-be/routes"
 )
 
@@ -9,10 +12,11 @@ func main() {
 	r := gin.Default()
 
 	routes.RegisterRoutes(r)
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
